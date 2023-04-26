7d7f066e3768ae6b40b44b981263796c8dce8dd3
bertrandszoghy <bertrandszoghy@gmail.com>
2023-04-26 14:30:02 -0400

    initial add

---
+++ src/main/java/ca/levio/entrevue/EntrevueApplication.java
@@ -0,0 +1,13 @@
+package ca.levio.entrevue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrevueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntrevueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/levio/entrevue/EntrevueApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.levio.entrevue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntrevueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
